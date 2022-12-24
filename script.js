65f5f857db215b3356be6865e6b763d5528c3e4d
Toby Kauenhowen <116322319+TOBY19k@users.noreply.github.com>
2022-12-24 17:50:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.querySelector(".install").addEventListener("click", GoToInstallerReleas
 function GoToInstallerReleasePage() {
   window.location.assign("https://github.com/TOBY19k/Seal-Os-Boot-Animation-Installer/releases/latest");
 }
+
 document.querySelector(".uninstall").addEventListener("click", GoToUninstallerReleasePage);
 function GoToUninstallerReleasePage() {
   window.location.assign("https://github.com/TOBY19k/Boot-Animation-Uninstaller/releases/latest");
